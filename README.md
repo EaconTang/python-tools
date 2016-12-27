47c6b2a9ad7a725a62c423a659f49b3016b7fa8c
EaconTang <tyingk@gmail.com>
2016-12-27 10:40:22 +0800

    init code_counter

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # python tools
 ---
 
-自己工作中使用的自制小工具
+日常使用的自制工具
 
 - ssh/pyssh.py
     - 治疗iTerm不能记录ssh连接、管理用户密码
     - 增加代理功能
+- code/code_counter.py
+    + 代码行数统计
---
+++ code/code_counter.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+"""
+python code_counter.py [OPTIONS] PROJECT_ROOT_DIR
+
+Options:
+-p        Python files only, deafult value
+-j        Java files only
+-g        Golang files only
+-c        C/C++ files only
+"""
+import sys
+import os
+import platform
+
+
+def main():
+    """"""
+    sys.argv = ['file', '-p', '.']
+    option, project_path = parse_args(*sys.argv[1:])
+    file_type = {
+        '-p': 'py',
+        '-j': 'java',
+        '-g': 'go',
+        '-c': '{c, cpp}'
+    }.get(option)
+    if platform.system() in ('Linux', 'Darwin'):
+        shell_cmd(project_path, file_type)
+    else:
+        pass
+
+
+
+def parse_args(*args):
+    """"""
+    options = ('-p', '-j', '-g', '-c')
+    default_option = '-p'
+    args_len = len(args)
+    if args_len == 1:
+        project_path = args[0]
+        option = default_option
+    elif args_len == 2:
+        option, project_path = args
+        if option not in options:
+            sys.stderr.write('Unknow option: "{}"\n'.format(option))
+            print(usage())
+            sys.exit(-1)
+    else:
+        sys.stderr.write('Wrong usage!\n')
+        print(usage())
+        sys.exit(-1)
+    return option, project_path
+
+
+def shell_cmd(_project_path, _suffix):
+    """"""
+    CMD = """find {path} -name "*.{suffix}" |xargs grep -v "^$"|wc -l"""
+    _cmd = CMD.format(path=_project_path, suffix=_suffix)
+    os.system(_cmd)
+
+
+def walk_dir(project_path):
+    """"""
+    pass
+
+
+def usage():
+    """"""      
+    return '\n'.join([
+        'Usage: ',
+        '\tpython code_counter.py [OPTIONS] PROJECT_ROOT_DIR'
+    ])
+
+
+if __name__ == '__main__':
+    main()
