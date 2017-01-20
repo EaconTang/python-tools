32f022adf54e57e19a0af64405c7cebdf4fbcec5
EaconTang <tyingk@gmail.com>
2017-01-20 18:51:14 +0800

    add features

--- README.md
+++ README.md
@@ -11,9 +11,10 @@
 - alarm-clock
     + 自制命令行闹钟
     + 专治懒人，必须起床敲命令行关闭闹钟
-    + 无限自定义闹铃
-    + 支持标签功能
+    + 标签功能
     + 支持定义只在工作日、周末、或者具体周一／周二闹铃
+    + 热更新
+    + 作为守护进程运行
         
         
 ### pyssh
@@ -73,19 +74,21 @@
 
     $ python alarm.py -h
     Usage:
-        python alarm.py [-r] [-l]
+        python alarm.py [options]
     
     Simple Alarm Clock
     
     Options:
       -h, --help            show this help message and exit
       -l, --list            list all alarm clocks
-      -a, --all             run all alarm clocks
-      -t RUN_LABEL, --tag=RUN_LABEL
+      -a, --all-run         run all alarm clocks
+      -t RUN_LABEL, --tag-only=RUN_LABEL
                             run alarm clocks by label
-      -k, --kill            kill all ringing tones
+      -k, --kill-clock      kill all clocks
+      --kill-music          kill ringing tones
+      -r, --reload          reload config from file: clock.json
       -d, --daemon          run as daemon
-     
+      --status              show status     
          
     # 查看闹钟配置         
     $ python alarm.py -l
@@ -107,26 +110,53 @@
             }
         ],
         "default_ringtone": "gao_bai_qi_qiu.mp3",
-        "default_filter": [
-            "everyday"
-        ],
+        "default_filter": ["everyday"],
         "default_status": "on",
         "default_label": "default",
         "default_ringtone_folder": "/Users/eacon/Documents/music"
     }   
           
-    # 开启闹钟(加&符后台运行)
-    $ python alarm.py -a &
-    [1] 80950
-    $ 
-    $ 
-    playing music: /Users/eacon/Documents/music/gao_bai_qi_qiu.mp3
+    # 开启闹钟(加-d作为守护进程运行，-a运行所有闹钟)
+    $ python alarm.py -a -d
+    # -t标签，指定只运行"work"的闹钟
+    $ python alarm.py -t work
     
-    [1]  + 80950 done       python alarm.py -a
+    # 关掉正在吵你的音乐
+    $ python alarm.py --kill-music
     
-    # 关掉闹铃
-    $ python alarm.py --kill
+    # 关掉所有闹钟
+    $ python alarm.py -k
     
+    # 修改闹钟配置后，重载配置
+    $ pythoon alarm.py -r
+    
+日志：
+
+    2017-01-20 18:28:22,573 [pid:699] [tid:140735241310208] [pyalarm] [INFO] Start as daemon process, pid file: /tmp/eacon-alarm.pid
+    2017-01-20 18:28:22,576 [pid:701] [tid:140735241310208] [pyalarm] [INFO] Run all alarm...
+    2017-01-20 18:28:22,577 [pid:701] [tid:140735241310208] [pyalarm] [INFO] Start a new loop...
+    2017-01-20 18:28:22,580 [pid:701] [tid:140735241310208] [pyalarm] [DEBUG] Clock(id:4425163024)-Thread: name:Thread-1, ident:None, interval:68, isAlive:<bound method _Timer.isAlive of <_Timer(Thread-1, initial)>>, finished:<threading._Event object at 0x107c813d0>, isDaemon:False,
+    2017-01-20 18:28:22,584 [pid:701] [tid:140735241310208] [pyalarm] [INFO] A new clock(id:4425519376) is started! Clock time:18:29:00, ringtone: /Users/eacon/Documents/music/gao_bai_qi_qiu.mp3
+    2017-01-20 18:28:22,585 [pid:701] [tid:140735241310208] [pyalarm] [DEBUG] Clock(id:4425519440)-Thread: name:Thread-2, ident:None, interval:93022, isAlive:<bound method _Timer.isAlive of <_Timer(Thread-2, initial)>>, finished:<threading._Event object at 0x107c815d0>, isDaemon:False,
+    2017-01-20 18:28:22,587 [pid:701] [tid:140735241310208] [pyalarm] [INFO] A new clock(id:4425519440) is started! Clock time:16:38:00, ringtone: /Users/eacon/Documents/music/gao_bai_qi_qiu.mp3
+    2017-01-20 18:28:22,588 [pid:701] [tid:140735241310208] [pyalarm] [DEBUG] Clock(id:4425519504)-Thread: name:Thread-3, ident:None, interval:96202, isAlive:<bound method _Timer.isAlive of <_Timer(Thread-3, initial)>>, finished:<threading._Event object at 0x107c817d0>, isDaemon:False,
+    2017-01-20 18:28:22,589 [pid:701] [tid:140735241310208] [pyalarm] [INFO] A new clock(id:4425519504) is started! Clock time:15:45:00, ringtone: /Users/eacon/Documents/music/gao_bai_qi_qiu.mp3
+    
+    2017-01-20 18:29:30,236 [pid:772] [tid:123145306509312] [pyalarm] [INFO] Clock(id:4425163024) is playing music: /Users/eacon/Documents/music/gao_bai_qi_qiu.mp3
+
+    2017-01-20 18:30:03,792 [pid:1003] [tid:140735241310208] [pyalarm] [INFO] Reload...
+    2017-01-20 18:30:03,792 [pid:1003] [tid:140735241310208] [pyalarm] [INFO] Start killing process...
+    2017-01-20 18:30:03,792 [pid:1003] [tid:140735241310208] [pyalarm] [INFO] Removing pid file...
+    2017-01-20 18:30:03,793 [pid:1003] [tid:140735241310208] [pyalarm] [INFO] Process is killed!
+    2017-01-20 18:30:03,793 [pid:1003] [tid:140735241310208] [pyalarm] [INFO] Start as daemon process, pid file: /tmp/eacon-alarm.pid
+    2017-01-20 18:30:03,796 [pid:1005] [tid:140735241310208] [pyalarm] [INFO] Run all alarm...
+    2017-01-20 18:30:03,796 [pid:1005] [tid:140735241310208] [pyalarm] [INFO] Start a new loop...
+    2017-01-20 18:30:03,799 [pid:1005] [tid:140735241310208] [pyalarm] [DEBUG] Clock(id:4393967888)-Thread: name:Thread-1, ident:None, interval:7, isAlive:<bound method _Timer.isAlive of <_Timer(Thread-1, initial)>>, finished:<threading._Event object at 0x105e6a3d0>, isDaemon:False,
+    2017-01-20 18:30:03,800 [pid:1005] [tid:140735241310208] [pyalarm] [INFO] A new clock(id:4393967888) is started! Clock time:18:30:10, ringtone: /Users/eacon/Documents/music/gao_bai_qi_qiu.mp3
+    2017-01-20 18:30:03,800 [pid:1005] [tid:140735241310208] [pyalarm] [DEBUG] Clock(id:4393967952)-Thread: name:Thread-2, ident:None, interval:93123, isAlive:<bound method _Timer.isAlive of <_Timer(Thread-2, initial)>>, finished:<threading._Event object at 0x105e6a5d0>, isDaemon:False,
+    2017-01-20 18:30:03,800 [pid:1005] [tid:140735241310208] [pyalarm] [INFO] A new clock(id:4393967952) is started! Clock time:16:38:00, ringtone: /Users/eacon/Documents/music/gao_bai_qi_qiu.mp3
+    2017-01-20 18:30:03,801 [pid:1005] [tid:140735241310208] [pyalarm] [DEBUG] Clock(id:4393968016)-Thread: name:Thread-3, ident:None, interval:96303, isAlive:<bound method _Timer.isAlive of <_Timer(Thread-3, initial)>>, finished:<threading._Event object at 0x105e6a7d0>, isDaemon:False,
+    2017-01-20 18:30:03,801 [pid:1005] [tid:140735241310208] [pyalarm] [INFO] A new clock(id:4393968016) is started! Clock time:15:45:00, ringtone: /Users/eacon/Documents/music/gao_bai_qi_qiu.mp3
 
 ### 建议用法
 赋予脚本执行权限：
@@ -137,16 +167,3 @@
     
     $ ln -s <path-to-alarm.py> </usr/local/bin/pyalarm>
     $ pyalarm -h
-    Usage:
-        python alarm.py [-r] [-l]
-    
-    Simple Alarm Clock
-    
-    Options:
-      -h, --help            show this help message and exit
-      -l, --list            list all alarm clocks
-      -a, --all             run all alarm clocks
-      -t RUN_LABEL, --tag=RUN_LABEL
-                            run alarm clocks by label
-      -k, --kill            kill all ringing tones
-      -d, --daemon          run as daemon
---
+++ clock/alarm.py
@@ -0,0 +1,316 @@
+#!/usr/bin/env python
+"""
+
+"""
+import os
+import json
+from optparse import OptionParser
+from threading import Timer
+import re
+import time
+import sys
+import atexit
+import signal
+import logging
+
+# make sure the access right
+CONFIG_FILE = '/Users/eacon/github/python-tools/clock/clock.json'
+PID_FILE = '/tmp/eacon-alarm.pid'
+LOG_FILE = '/var/log/eacon-alarm.log'
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.DEBUG,
+    format="%(asctime)-15s [pid:%(process)d] [tid:%(thread)d] [%(filename)s] [%(levelname)s] %(message)s",
+)
+LOG = logging.getLogger(__name__)
+
+
+def main():
+    """"""
+    options = parse_args()
+    if options.list_clock:
+        LOG.info('List all alarm...')
+        list_clocks()
+    if options.status:
+        show_status()
+
+    if options.reload_conf:
+        LOG.info('Reload...')
+        stop_clocks()
+        # defaults to daemon
+        if not options.daemonize:
+            options.daemonize = True
+        # if not specified, defaults to run all
+        if not options.run_all_clock and options.run_label is None:
+            options.run_all_clock = True
+
+    if options.daemonize:
+        daemonize()
+    if options.kill_ringtones:
+        LOG.info('Stop music...')
+        stop_music()
+        return
+    if options.kill_clocks:
+        LOG.info('Stop all clocks...')
+        stop_clocks()
+        return
+    if options.run_all_clock:
+        LOG.info('Run all alarm...')
+        loop_forever(24 * 60 * 60, run_clocks, func_args=['all'])
+        return
+    if options.run_label is not None:
+        LOG.info('Run alarm with label: {}'.format(options.run_label))
+        loop_forever(24 * 60 * 60, run_clocks, func_args=[str(options.run_label)])
+        return
+
+
+def parse_args():
+    """"""
+    parser = OptionParser(usage="\n\tpython alarm.py [options]",
+                          prog="alarm",
+                          description="Simple Alarm Clock",
+                          add_help_option=True)
+    parser.add_option('-l', '--list', dest='list_clock', action='store_true', default=False,
+                      help='list all alarm clocks')
+    parser.add_option('-a', '--all-run', dest='run_all_clock', action='store_true', default=False,
+                      help='run all alarm clocks')
+    parser.add_option('-t', '--tag-only', dest='run_label',
+                      help='run alarm clocks by label')
+    parser.add_option('-k', '--kill-clock', dest='kill_clocks', action='store_true', default=False,
+                      help='kill all clocks')
+    parser.add_option('--kill-music', dest='kill_ringtones', action='store_true', default=False,
+                      help='kill ringing tones')
+    parser.add_option('-r', '--reload', dest='reload_conf', action='store_true', default=False,
+                      help='reload config from file: clock.json')
+    parser.add_option('-d', '--daemon', dest='daemonize', action='store_true', default=False,
+                      help='run as daemon')
+    parser.add_option('--status', dest='status', action='store_true', default=False,
+                      help='show status')
+    (options, args) = parser.parse_args()
+    return options
+
+
+def load_conf():
+    """"""
+    with open(CONFIG_FILE) as f:
+        clock_dict = json.load(f)
+        return clock_dict
+
+
+def list_clocks():
+    """"""
+    clocks = load_conf()
+    print json.dumps(clocks, indent=4)
+
+
+def loop_forever(interval, func, func_args=None, func_kwargs=None, callback=None):
+    """
+    Mainloop
+    :param interval: loop interval, 1 day in this script
+    :param func:
+    :param func_args:
+    :param func_kwargs:
+    :param callback: callback function after loop function is executed
+    :type callback: dict
+        {func_name: , func_args: , func_kwargs: }
+    :return:
+    """
+    if func_args is None:
+        func_args = []
+    if func_kwargs is None:
+        func_kwargs = {}
+    error_count = 0
+    while True:
+        try:
+            LOG.info('Start a new loop...')
+            func(*func_args, **func_kwargs)
+            time.sleep(interval)
+            LOG.info('Finish one loop...')
+            if callback:
+                eval(callback['func_name'])(*callback.get('func_args', []), **callback.get('func_kwargs', {}))
+        except Exception as e:
+            LOG.error(e.message)
+            error_count += 1
+            if error_count <= 3:
+                continue
+            else:
+                LOG.error('Too many errors, we will exit the loop.')
+                break
+
+
+def run_clocks(label='all'):
+    """
+    start all clocks
+    :param label: specified label, as a filter
+    :return:
+    """
+    conf = load_conf()
+    clocks = [Clock(clock, conf) for clock in conf['clocks']]
+    clocks = [c for c in clocks if c.status == 'on' and c.istoday]
+    if label.lower() != 'all':
+        clocks = [c for c in clocks if c.label.lower() == label.lower()]
+    for c in clocks:
+        c.start()
+
+
+def stop_clocks():
+    """
+    actually kill the process
+    :return:
+    """
+    LOG.info('Start killing process...')
+    with open(PID_FILE) as f:
+        os.kill(int(f.read()), signal.SIGKILL)
+    LOG.info('Removing pid file...')
+    os.remove(PID_FILE)
+    LOG.info('Process is killed!')
+
+
+def show_status():
+    """
+    see whether the clock is on
+    :return:
+    """
+    if os.path.exists(PID_FILE):
+        print 'Alarm is active.'
+    else:
+        print 'Alarm is inactive.'
+
+
+def stop_music():
+    """
+    stop ringing music
+    :return:
+    """
+    LOG.info('Kill playing music...')
+    CMD = """ps aux|grep afplay|grep -v "grep"|awk '{print $2}'|xargs kill -9"""
+    os.system(CMD)
+
+
+def daemonize():
+    """
+    make it daemon process
+    :return:
+    """
+    LOG.info('Start as daemon process, pid file: {}'.format(PID_FILE))
+    pid = os.fork()
+    if pid:
+        sys.exit(0)
+
+    os.chdir('/')
+    os.umask(0)
+    os.setsid()
+
+    _pid = os.fork()
+    if _pid:
+        sys.exit(0)
+
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    with open('/dev/null') as read_null, open('/dev/null', 'w') as write_null:
+        os.dup2(read_null.fileno(), sys.stdin.fileno())
+        os.dup2(write_null.fileno(), sys.stdout.fileno())
+        os.dup2(write_null.fileno(), sys.stderr.fileno())
+
+    if PID_FILE:
+        with open(PID_FILE, 'w+') as f:
+            f.write(str(os.getpid()))
+        atexit.register(os.remove, PID_FILE)
+
+
+class Clock(object):
+    """
+    Each clock instance as a thread
+    """
+    def __init__(self, clock, defaults):
+        self._ringtone_folder = clock.get('ringtone_folder', defaults['default_ringtone_folder'])
+        self._ringtone = clock.get('ringtone', defaults['default_ringtone'])
+        self._filter = clock.get('filter', defaults['default_filter'])
+        self._status = clock.get('status', defaults['default_status'])
+        self._label = clock.get('label', defaults['default_label'])
+        self._time = clock.get('time', -1)
+
+    def start(self):
+        """start a new thread(Timer)"""
+        countdown = self.get_count_down(self._time)
+        music_path = os.path.join(self._ringtone_folder, self._ringtone)
+        if countdown >= 0:
+            t = Timer(countdown, self.play_music, [music_path])
+            LOG.debug(
+                'Clock(id:{})-Thread: name:{}, ident:{}, interval:{}, isAlive:{}, finished:{}, isDaemon:{}, '.format(
+                    id(self), t.getName(), t.ident, t.interval, t.is_alive, t.finished, t.isDaemon()
+                )
+            )
+            t.start()
+            LOG.info('A new clock(id:{}) is started! Clock time:{}, ringtone: {}'.format(
+                id(self), self._time, music_path
+            ))
+
+    @property
+    def label(self):
+        return self._label
+
+    @property
+    def status(self):
+        return self._status
+
+    @property
+    def istoday(self):
+        """filter day"""
+        _, _, _, _, _, _, tm_wday, _, _ = time.localtime()
+        d = {
+            'mon': [0],
+            'tue': [1],
+            'wed': [2],
+            'thu': [3],
+            'fri': [4],
+            'sat': [5],
+            'sun': [6],
+            'weekday': range(5),
+            'weekend': range(5, 7),
+            'everyday': range(7),
+        }
+        res = []
+        for _ in self._filter:
+            if tm_wday in d.get(_.lower(), []):
+                return True
+
+    @staticmethod
+    def get_count_down(time_str):
+        """
+        count alarm time from now
+        :param time_str:
+        :type time_str: str
+        :return:
+        """
+        if re.match(r'\d+:\d+:\d+', time_str):
+            h, m, s = time_str.split(':')
+        elif re.match(r'\d+:\d+', time_str):
+            h, m = time_str.split(':')
+            s = 0
+        else:
+            raise TypeError("Wrong type for time:" + str(time_str))
+        clock_timestamp = sum((int(s), int(m) * 60, int(h) * 60 * 60))
+
+        _, _, _, tm_hour, tm_min, tm_sec, _, _, _ = time.localtime()
+        now_timestamp = sum((tm_sec, tm_min * 60, tm_hour * 60 * 60))
+
+        countdown = clock_timestamp - now_timestamp
+        if countdown < 0:
+            countdown = 24 * 60 * 60 - countdown
+        return countdown
+
+    def play_music(self, music_path):
+        """
+        actually play music, by "afplay"
+        :param music_path:
+        :return:
+        """
+        os.system("afplay {_musix_path} &".format(_musix_path=music_path))
+        LOG.info('Clock(id:{}) is playing music: {}'.format(id(self), music_path))
+
+
+if __name__ == '__main__':
+    main()
